86a8cee8437b4602f6df684af3ec64c386a34466
Nishant paliwal <paliwal.nishant1@gmail.com>
2015-08-24 01:47:16 +0530

    Update jobrun.py

--- jobrun.py
+++ jobrun.py
@@ -4,16 +4,16 @@
 
 import os 
 
-#os.system('dialog  --inputbox  "Enter the word which you want to count :" 75 75  2>/tmp/wcount.txt')
+os.system('dialog  --inputbox  "Enter the word which you want to count :" 75 75  2>/tmp/wcount.txt')
 os.system('dialog  --inputbox  "Enter the path of file on hdfs system :"   50 50  2>/tmp/location.txt')
 
 f1=open("/tmp/client.txt",'r')
 client=f1.read()
 f1.close()
 
-#f1=open("/tmp/wcount.txt",'r')
-#word=f1.read()
-#f1.close()
+f1=open("/tmp/wcount.txt",'r')
+word=f1.read()
+f1.close()
 
 f1=open("/tmp/location.txt",'r')
 location=f1.read()
